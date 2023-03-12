613d32943cc06da3627737eb1fd28be71f1a0189
szyper <zskgit@gmail.com>
2023-03-12 14:38:35 +0100

    php db

---
+++ 2_db/1_db_table.php
@@ -0,0 +1,35 @@
+<!doctype html>
+<html lang=pl>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Użytkownicy</title>
+</head>
+<body>
+<h4>Użytkownicy</h4>
+<?php
+    require_once "./scripts/connect.php";
+    echo <<<TABLEHEAD
+        <table>
+            <tr>
+                <th>Imię</th>
+                <th>Nazwisko</th>
+                <th>Data urodzenia</th>
+            </tr>
+TABLEHEAD;
+
+    $sql = "SELECT * FROM `users`;";
+    $result = $conn->query($sql);
+    while($user = $result->fetch_assoc()){
+	    echo <<< USERS
+            <tr>
+                <td>$user[firstName]</td>
+            </tr>
+USERS;
+    }
+echo "</table>";
+?>
+</body>
+</html>
---
+++ 2_db/scripts/connect.php
@@ -0,0 +1,3 @@
+<?php
+	$conn = new mysqli("localhost", "root", "", "wsb_niinz_3_5s_22-23_inf_sp2_j-zyki-programowania-php-2");
+//	echo "db";
