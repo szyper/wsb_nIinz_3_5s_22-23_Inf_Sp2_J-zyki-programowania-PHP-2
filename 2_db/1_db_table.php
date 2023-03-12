d12fa4076cc40eaf40b50bec94bba8510a6e99b5
szyper <zskgit@gmail.com>
2023-03-12 14:56:18 +0100

    php db

--- 2_db/1_db_table.php
+++ 2_db/1_db_table.php
@@ -5,6 +5,7 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="./style/table.css">
     <title>Użytkownicy</title>
 </head>
 <body>
@@ -17,15 +18,21 @@
                 <th>Imię</th>
                 <th>Nazwisko</th>
                 <th>Data urodzenia</th>
+                <th>Miasto</th>
+                <th>Województwo</th>
             </tr>
 TABLEHEAD;
 
-    $sql = "SELECT * FROM `users`;";
+    $sql = "SELECT * FROM `users` INNER JOIN `cities` ON `users`.`city_id`=`cities`.`id` INNER JOIN `states` ON `cities`.`state_id`=`states`.`id`;;";
     $result = $conn->query($sql);
     while($user = $result->fetch_assoc()){
 	    echo <<< USERS
             <tr>
                 <td>$user[firstName]</td>
+                <td>$user[lastName]</td>
+                <td>$user[birthday]</td>
+                <td>$user[cityName]</td>
+                <td>$user[stateName]</td>
             </tr>
 USERS;
     }
